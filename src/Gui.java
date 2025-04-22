6f240287e363b9164cab78c542fd7e329ebf05a4
Om <ompanem@gmail.com>
2025-04-22 17:39:42 -0500

    User able to write txt files and save it on their computer

---
+++ src/Gui.java
@@ -0,0 +1,96 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class Gui {
+    final static int SCREEN_WIDTH = 500;
+    final static int SCREEN_HEIGHT = 500;
+    static JButton saveButton;
+    static JButton openButton;
+    static JTextArea textArea;
+    static JFrame jFrame;
+    static int counter = 1;
+    public static void initFrame() {
+         jFrame = new JFrame("Notepad");
+        jFrame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.add(getPanel());
+        jFrame.setVisible(true);
+    }
+
+    public static JPanel getPanel() {
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new BorderLayout());
+        jPanel.add(getButtonsPanel(), BorderLayout.NORTH);
+        textArea = new JTextArea("Type here: ");
+        jPanel.add(textArea, BorderLayout.CENTER);
+        return jPanel;
+    }
+
+    public static JPanel getButtonsPanel()
+    {
+        JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+         saveButton = new JButton("Save");
+         openButton = new JButton("Open");
+        addButtonListeners();
+        buttonsPanel.add(saveButton);
+        buttonsPanel.add(openButton);
+
+        return buttonsPanel;
+
+    }
+
+    public static void addButtonListeners()
+    {
+        saveButton.addActionListener(getSaveButtonListener());
+    }
+
+    public static ActionListener getSaveButtonListener()
+    {
+        return (new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String path = System.getProperty("user.home");
+                String text = textArea.getText();
+                File downloads = new File(path, "Downloads");
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setCurrentDirectory(downloads);
+                fileChooser.setFileFilter(new FileFilter() {
+                    @Override
+                    public boolean accept(File f) {
+                        return f.getName().endsWith(".txt");
+                    }
+
+                    @Override
+                    public String getDescription() {
+                        return "";
+                    }
+                });
+                fileChooser.setDialogTitle("Save File");
+                int res = fileChooser.showSaveDialog(null);
+                if(res == JFileChooser.APPROVE_OPTION)
+                {
+                    File textFile = new File(fileChooser.getSelectedFile().getAbsolutePath() + ".txt"); //if txt is appended then it will write the contents to a txt file
+                    try(BufferedWriter writer = new BufferedWriter(new FileWriter(textFile))){
+                        writer.write(text);
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    final static int SCREEN_WIDTH = 500;
+    final static int SCREEN_HEIGHT = 500;
+
+    public static void main(String[] args) {
+        Gui.initFrame();
+    }
+
+
+}
